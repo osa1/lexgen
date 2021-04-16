3696bafed72e817b651c89cb9b5565c7fed40ac3
Ömer Sinan Ağacan <omeragacan@gmail.com>
2021-04-16 11:45:45 +0300

    Add NFA simulation and tests

--- src/nfa.rs
+++ src/nfa.rs
@@ -79,3 +79,166 @@ impl NFA {
         self.accepting.insert(state);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    pub(crate) fn simulate(nfa: &NFA, chars: &mut dyn Iterator<Item = char>) -> bool {
+        let mut states: FxHashSet<StateIdx> = Default::default();
+        states.insert(StateIdx(0));
+        compute_state_closure(nfa, &mut states);
+
+        for char in chars {
+            // println!("char = {}", char);
+            // println!("states = {:?}", states);
+
+            let mut next_states: FxHashSet<StateIdx> = Default::default();
+            for state in states.iter() {
+                if let Some(nexts) = next_char_states(nfa, *state, char) {
+                    next_states.extend(nexts.into_iter());
+                }
+                for ((range_begin, range_end), nexts) in &nfa.states[state.0].range_transitions {
+                    if char >= *range_begin && char <= *range_end {
+                        next_states.extend(nexts.into_iter());
+                    }
+                }
+            }
+
+            compute_state_closure(nfa, &mut next_states);
+
+            states = next_states;
+        }
+
+        states.iter().any(|state| nfa.accepting.contains(state))
+    }
+
+    fn compute_state_closure(nfa: &NFA, states: &mut FxHashSet<StateIdx>) {
+        let mut changed = true;
+        while changed {
+            changed = false;
+
+            let mut next = states.clone();
+
+            for state in states.iter() {
+                for next_state in next_empty_states(nfa, *state) {
+                    changed |= next.insert(*next_state);
+                }
+            }
+
+            *states = next;
+        }
+    }
+
+    fn next_char_states(nfa: &NFA, state: StateIdx, char: char) -> Option<&FxHashSet<StateIdx>> {
+        let state = &nfa.states[state.0];
+        state.char_transitions.get(&char)
+    }
+
+    fn next_empty_states(nfa: &NFA, state: StateIdx) -> &FxHashSet<StateIdx> {
+        let state = &nfa.states[state.0];
+        &state.empty_transitions
+    }
+
+    use crate::ast::{CharOrRange, CharSet, Regex};
+    use crate::regex_to_nfa::regex_to_nfa;
+
+    #[test]
+    fn nfa_simulate_char() {
+        let re = Regex::Char('a');
+        let nfa = regex_to_nfa(&re);
+        assert!(!simulate(&nfa, &mut "".chars()));
+        assert!(simulate(&nfa, &mut "a".chars()));
+    }
+
+    #[test]
+    fn nfa_simulate_string() {
+        let re = Regex::String("ab".to_owned());
+        let nfa = regex_to_nfa(&re);
+        assert!(!simulate(&nfa, &mut "".chars()));
+        assert!(!simulate(&nfa, &mut "a".chars()));
+        assert!(simulate(&nfa, &mut "ab".chars()));
+    }
+
+    #[test]
+    fn nfa_simulate_char_set_char() {
+        let re = Regex::CharSet(CharSet(vec![
+            CharOrRange::Char('a'),
+            CharOrRange::Char('b'),
+        ]));
+        let nfa = regex_to_nfa(&re);
+        assert!(!simulate(&nfa, &mut "".chars()));
+        assert!(simulate(&nfa, &mut "a".chars()));
+        assert!(simulate(&nfa, &mut "b".chars()));
+        assert!(!simulate(&nfa, &mut "ab".chars()));
+        assert!(!simulate(&nfa, &mut "ba".chars()));
+    }
+
+    #[test]
+    fn nfa_simulate_char_set_range() {
+        let re = Regex::CharSet(CharSet(vec![
+            CharOrRange::Char('a'),
+            CharOrRange::Char('b'),
+            CharOrRange::Range('0', '9'),
+        ]));
+        let nfa = regex_to_nfa(&re);
+        assert!(!simulate(&nfa, &mut "".chars()));
+        assert!(simulate(&nfa, &mut "a".chars()));
+        assert!(simulate(&nfa, &mut "b".chars()));
+        assert!(simulate(&nfa, &mut "0".chars()));
+        assert!(simulate(&nfa, &mut "1".chars()));
+        assert!(simulate(&nfa, &mut "9".chars()));
+        assert!(!simulate(&nfa, &mut "ba".chars()));
+    }
+
+    #[test]
+    fn nfa_simulate_zero_or_more() {
+        let re = Regex::ZeroOrMore(Box::new(Regex::Char('a')));
+        let nfa = regex_to_nfa(&re);
+        assert!(simulate(&nfa, &mut "".chars()));
+        assert!(simulate(&nfa, &mut "a".chars()));
+        assert!(simulate(&nfa, &mut "aa".chars()));
+        assert!(!simulate(&nfa, &mut "aab".chars()));
+    }
+
+    #[test]
+    fn nfa_simulate_one_or_more() {
+        let re = Regex::OneOrMore(Box::new(Regex::Char('a')));
+        let nfa = regex_to_nfa(&re);
+        assert!(!simulate(&nfa, &mut "".chars()));
+        assert!(simulate(&nfa, &mut "a".chars()));
+        assert!(simulate(&nfa, &mut "aa".chars()));
+        assert!(!simulate(&nfa, &mut "aab".chars()));
+    }
+
+    #[test]
+    fn nfa_simulate_zero_or_one() {
+        let re = Regex::ZeroOrOne(Box::new(Regex::Char('a')));
+        let nfa = regex_to_nfa(&re);
+        assert!(simulate(&nfa, &mut "".chars()));
+        assert!(simulate(&nfa, &mut "a".chars()));
+        assert!(!simulate(&nfa, &mut "aa".chars()));
+    }
+
+    #[test]
+    fn nfa_simulate_concat() {
+        let re = Regex::Concat(Box::new(Regex::Char('a')), Box::new(Regex::Char('b')));
+        let nfa = regex_to_nfa(&re);
+        assert!(!simulate(&nfa, &mut "".chars()));
+        assert!(!simulate(&nfa, &mut "a".chars()));
+        assert!(simulate(&nfa, &mut "ab".chars()));
+        assert!(!simulate(&nfa, &mut "aba".chars()));
+        assert!(!simulate(&nfa, &mut "abb".chars()));
+    }
+
+    #[test]
+    fn nfa_simulate_or() {
+        let re = Regex::Or(Box::new(Regex::Char('a')), Box::new(Regex::Char('b')));
+        let nfa = regex_to_nfa(&re);
+        assert!(!simulate(&nfa, &mut "".chars()));
+        assert!(simulate(&nfa, &mut "a".chars()));
+        assert!(simulate(&nfa, &mut "b".chars()));
+        assert!(!simulate(&nfa, &mut "aa".chars()));
+        assert!(!simulate(&nfa, &mut "ab".chars()));
+    }
+}
