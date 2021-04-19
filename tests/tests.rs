e0389a8ef470c3d695a0c73da27aefc3483cce49
Ömer Sinan Ağacan <omeragacan@gmail.com>
2021-04-19 14:51:39 +0300

    Add a switch test, does not work yet

--- tests/tests.rs
+++ tests/tests.rs
@@ -17,10 +17,6 @@ fn simple() {
         [' ' '\t' '\n']+,
 
         $init $subseq* => |str: &str| LexerAction::Return(Token::Id(str.to_owned())),
-
-        rule Blah {
-            $init => |str: &str| todo!(),
-        }
     }
 
     let mut lexer = Lexer::new(" abc123Q-t  z9_9");
@@ -33,4 +29,28 @@ fn simple() {
         Some(Ok((12, Token::Id("z9_9".to_owned()), 16)))
     );
     assert_eq!(lexer.next(), None);
+}
+
+#[test]
+fn switch() {
+    #[derive(Debug, PartialEq, Eq)]
+    enum Token {
+        Comment,
+    }
+
+    lexer_gen! {
+        Lexer -> Token;
+
+        // Whitespace
+        [' ' '\t' '\n']+,
+
+        "/*" => |_: &str| LexerAction::Switch(LexerRules::Comment),
+
+        rule Comment {
+            "*/" => |_: &str| LexerAction::Return(Token::Comment),
+        }
+    }
+
+    let mut lexer = Lexer::new(" /*   */");
+    assert_eq!(lexer.next(), Some(Ok((1, Token::Comment, 8))));
 }
